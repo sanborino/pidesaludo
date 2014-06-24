de5a841384ae4f24043fac6fd2fe578f832f1114
sanborino <Ricardo@MacBook-Pro-de-Ricardo.local>
2014-06-23 21:21:09 -0600

    Initial Commit

---
+++ pidesaludo/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  pidesaludo
+//
+//  Created by Ricardo A Mondragon A on 23/06/14.
+//  Copyright (c) 2014 Ricardo A Mondragon A. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
